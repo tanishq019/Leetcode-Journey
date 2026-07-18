e7a0795da7b73e66870b5ca68d9cbf885293eaba
Tanishq Gupta <148804620+tanishq019@users.noreply.github.com>
2026-07-19 01:13:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.42%) - LeetSync

---
+++ 770-couples-holding-hands/couples-holding-hands.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minSwapsCouples(self, row: list[int]) -> int:
+        swaps = 0
+        n = len(row)
+        
+        for i in range(0, n, 2):
+            partner = row[i] ^ 1
+            
+            if row[i+1] != partner:
+ 
+                partner_idx = row.index(partner, i + 1)
+
+                row[i+1], row[partner_idx] = row[partner_idx], row[i+1]
+                swaps += 1
+                
+        return swaps
