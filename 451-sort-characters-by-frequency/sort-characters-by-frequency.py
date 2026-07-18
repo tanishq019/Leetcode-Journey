65bffc651705825df46bcf2acdbd474438ba9444
Tanishq Gupta <148804620+tanishq019@users.noreply.github.com>
2026-07-19 00:52:12 +0530

    Time: 7 ms (79.55%) | Memory: 20.3 MB (42.46%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -7,4 +7,4 @@ class Solution:
         for ch, count in sorted(freq.items(), key = lambda x: x[1], reverse=True):
             result.append(ch * count)
 
-        return ''.join(result)    
+        return ''.join(result)
