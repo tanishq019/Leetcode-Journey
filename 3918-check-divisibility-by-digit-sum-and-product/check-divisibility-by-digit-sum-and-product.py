70cf9606266ed90b74a5cf81f584b41b51ba8662
Tanishq Gupta <148804620+tanishq019@users.noreply.github.com>
2026-08-22 21:39:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (58.35%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.py
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.py
@@ -2,14 +2,10 @@ class Solution:
     def checkDivisibility(self, n: int) -> bool:
         summ = 0
         prod = 1
-        temp = n
+        original = n
         while n > 0 :
             last_digit = n % 10
             summ += last_digit
             prod *= last_digit
             n //= 10
-        total = summ + prod
-        if temp % total == 0:
-            return True
-        else:
-            return False
+        return original % (summ + prod) == 0
